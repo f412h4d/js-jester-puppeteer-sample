a5b59b71ec1da3e4680846b993562aa7f684f06f
f4rh4d <farhadf4rh4di@gmail.com>
2020-05-30 18:01:02 +0430

    added an e2e test to check over all process

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const {generateText, checkAndGenerate} = require('./util');
 
 // Unit Tests
@@ -18,3 +19,30 @@ test('should generate a valid text', () => {
     expect(text).toBe('Amir (21 years old)');
 })
 //Integration Tests
+
+// e2e Tests
+test('should create an element with correct text', async () => {
+    const browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 80,
+        }
+    );
+    const page = await browser.newPage();
+
+    await page.goto(
+        'file:///home/f4rh4d/Projects/js-testing-introduction/index.html'
+    );
+
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+    await page.click('input#age');
+    await page.type('input#age', '28');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Anna (28 years old)');
+
+    await page.screenshot({path: '/home/f4rh4d/tests/example.png'});
+
+    await browser.close();
+}, 10000);
+// e2e Tests
