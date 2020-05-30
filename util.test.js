1a9c2e29d68043d0f1bbc0a70b38c1ad9d67e82c
f4rh4d <farhadf4rh4di@gmail.com>
2020-05-30 17:59:30 +0430

    added comments and fixed the first unit test

--- util.test.js
+++ util.test.js
@@ -1,6 +1,13 @@
 const {generateText} = require('./util');
 
+// Unit Tests
 test('should out put name and age', () => {
     const text = generateText('Amir', '21');
-    expect(text).toBe('Amir (21 years old)')
+    expect(text).toBe('Amir (21 years old)');
 });
+
+test('test for null values', () => {
+    const text = generateText();
+    expect(text).toBe('undefined (undefined years old)');
+})
+// Unit Tests
